0be5a24cda2b1467b1a63e13da4278ffefd8dd0c
JM-DESKTOP\admin <miggysvll@gmail.com>
2021-03-15 22:31:26 +0800

    Sync Method PHP Backend Activity 01

---
+++ components/inputs/inputs.php
@@ -0,0 +1,28 @@
+<div class="form-outline" style="margin-bottom: 30px;">
+    <input type="text" id="txtfirstname" class="form-control" />
+    <label class="form-label" for="form1">Firstname</label>
+</div>
+
+<div class="form-outline" style="margin-bottom: 30px;">
+    <input type="text" id="txtfirstname" class="form-control" />
+    <label class="form-label" for="form1">Lastname</label>
+</div>
+
+<div class="form-outline" style="margin-bottom: 30px;">
+    <input type="text" id="txtfirstname" class="form-control" />
+    <label class="form-label" for="form1">Nickname</label>
+</div>
+
+<div class="form-outline" style="margin-bottom: 30px;">
+    <input type="text" id="txtfirstname" class="form-control" />
+    <label class="form-label" for="form1">Favorite super hero</label>
+</div>
+
+<div class="form-outline" style="margin-bottom: 30px;">
+    <textarea class="form-control" id="textAreaExample" rows="4"></textarea>
+    <label class="form-label" for="textAreaExample">Address</label>
+</div>
+
+
+
+<button id="onsave" class="btn btn-primary" style="float:right; margin-bottom: 20px;">Save</button>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <?php include("includes/links/links.php"); ?>
+</head>
+
+<body>
+<?php include("components/navigation/navbar.php"); ?>
+<?php include("views/indexviews.php"); ?>
+<?php include("includes/scripts/scripts.php"); ?>
+</body>
+</html>
